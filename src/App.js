ff52aaf9c5130ccbcb14ae85e5e7534b5590070f
smijk <joseph.smith@tessella.com>
2021-07-21 14:03:30 +0100

    Updated with PR changes

--- src/App.js
+++ src/App.js
@@ -2,30 +2,33 @@ import { useState } from 'react'
 import TaskList from './components/TaskList'
 
 function App() {
-  const [tasks] = useState([
-      {
-          id: 1,
-          text: "Finish JIRA boards"
-      },
-      {
-          id: 2,
-          text: "Update readme to reflect changes"
-      },
-      {
-          id:3,
-          text: "Test ML model on new data"
-      },
-      {
-          id:4,
-          text: "Submit holiday request"
-      }  
-    ]);
+
+  var initialState = [
+    {
+      id: 1,
+      text: "Finish JIRA boards"
+    },
+    {
+      id: 2,
+      text: "Update readme to reflect changes"
+    },
+    {
+      id:3,
+      text: "Test ML model on new data"
+    },
+    {
+      id:4,
+      text: "Submit holiday request"
+    }
+  ];
+
+  const [tasks] = useState(initialState);
 
   return (
     <div className="App">
-      <TaskList task_array={tasks}/>
+      <TaskList taskArray={tasks}/>
     </div>
-  );
-}
+  )
+};
 
 export default App;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,13 +3,13 @@ import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 
-const TaskList = ({ task_array }) => {
-    if (!task_array || !task_array.length) {
-        return <div>"No tasks"</div>
+const TaskList = ({ taskArray }) => {
+    if (!taskArray || !taskArray.length) {
+        return <div>No tasks</div>
     }
     return (
         <List>
-            {task_array.map((task, id) => (
+            {taskArray.map((task, id) => (
                 <ListItem key={id}>
                     <ListItemText id={id} primary={task.text} data-testid='task'/>
                 </ListItem>
