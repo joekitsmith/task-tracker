ad8dbe34a2d145875241e517ce4d70255e2e634e
Joseph Smith <joseph.smith@tessella.com>
2021-07-21 15:05:11 +0100

    Add task button added below list

---
+++ src/components/AddTask.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from "@material-ui/core/Button"
+
+function AddTask() {
+
+    return (
+        <div>
+            <Button>
+                Add task
+            </Button>
+        </div>
+    );
+}
+
+export default AddTask
