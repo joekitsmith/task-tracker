9f1b6dbff6b2b9b17c3eb1a6ae4f56271d7a2e96
Joseph Smith <joseph.smith@tessella.com>
2021-07-21 15:23:00 +0100

    Add task dialog box with text input

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import Button from "@material-ui/core/Button"
+import Dialog from "@material-ui/core/Dialog"
+import DialogTitle from "@material-ui/core/DialogTitle"
+import DialogContent from '@material-ui/core/DialogContent'
+import TextField from "@material-ui/core/TextField"
 
 function AddTask() {
 
+    const [open, setOpen] = React.useState(false)
+
+    const handleClickOpen = () => {
+        setOpen(true)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
+
     return (
         <div>
-            <Button>
+            <Button onClick={handleClickOpen}>
                 Add task
             </Button>
+            <Dialog open ={open} onClose={handleClose}>
+                <DialogTitle>
+                    Add task
+                </DialogTitle>
+                <DialogContent>
+                    <TextField id="new_task" style={{ margin:20 }}/>
+                </DialogContent>
+            </Dialog>
         </div>
-    );
+    )
 }
 
 export default AddTask
