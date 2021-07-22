ba967c9b93b931499d5e29ca66efbf84e85314bd
Joseph Smith <joseph.smith@tessella.com>
2021-07-22 10:57:16 +0100

    Missing bracket

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,5 +3,5 @@ import App from '../App';
 
 test("updateTask function called", () => {
   // How to check that updateTasks function adds task to tasks array? //
-  const app = render(<App/>)
-}
+  render(<App/>)
+})
