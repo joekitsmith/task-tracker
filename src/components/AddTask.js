62f4a4ae78fe8fd1e4f0b82e12a1d6daea4f601e
Joseph Smith <joseph.smith@tessella.com>
2021-07-22 10:25:48 +0100

    Test id's and removed in-line styles

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import Button from "@material-ui/core/Button"
 import Dialog from "@material-ui/core/Dialog"
 import DialogTitle from "@material-ui/core/DialogTitle"
 import DialogContent from '@material-ui/core/DialogContent'
 import TextField from "@material-ui/core/TextField"
 import DialogActions from '@material-ui/core/DialogActions'
+import { mergeClasses } from '@material-ui/styles'
+
+const useStyles = makeStyles({
+    text: {
+        margin: 15
+    },
+})
 
 function AddTask({ onAdd }) {
+
+    const classes = useStyles()
 
     const [open, setOpen] = React.useState(false)
     const [text, setText] = React.useState("")
@@ -26,21 +36,22 @@ function AddTask({ onAdd }) {
 
     return (
         <div>
-            <Button onClick={handleOpen}>
+            <Button onClick={handleOpen} data-testid='dialogButton'>
                 Add task
             </Button>
-            <Dialog open ={open} onClose={handleClose}>
+            <Dialog open ={open} onClose={handleClose} data-testid='dialog'>
                 <DialogTitle>
                     Add task
                 </DialogTitle>
                 <DialogContent>
                     <TextField 
-                        style={{ margin:15 }}
+                        className={classes.text}
                         onChange={(event)=>{setText(event.target.value)}}
+                        data-testid='newTask'
                         />
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleAdd}>
+                    <Button onClick={handleAdd} data-testid='addButton'>
                         Add
                     </Button>
                 </DialogActions>
