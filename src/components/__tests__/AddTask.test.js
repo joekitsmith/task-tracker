46a0f8939c0feea2567e7ae170b78eeb66ad0652
Joseph Smith <joseph.smith@tessella.com>
2021-07-23 10:16:34 +0100

    Removed test for dialog closing

--- src/components/__tests__/AddTask.test.js
+++ src/components/__tests__/AddTask.test.js
@@ -7,7 +7,7 @@ test("dialog opens when Add Task button clicked", () => {
     const { getByTestId } = render(<AddTask/>)
     fireEvent.click(getByTestId('dialogButton'))
     const dialog = getByTestId('dialog')
-    expect(dialog).toBeInTheDocument()
+    expect(dialog).toHaveFocus()
 })
 
 test("input of new task string updates text field", () => {
@@ -16,18 +16,6 @@ test("input of new task string updates text field", () => {
     const textField = getByTestId('newTask').querySelector('input')
     fireEvent.change(textField, {target: { value: "example task"}})
     expect(textField.value).toBe("example task")
-})
-
-test("click Add button with filled text field closes dialog", () => {
-    // Passes but should fail as dialog should close //
-    const mock = jest.fn()
-    const { getByTestId } = render(<AddTask onAdd={mock}/>)
-    fireEvent.click(getByTestId('dialogButton'))
-    const dialog = getByTestId('dialog')
-    const textField = getByTestId('newTask').querySelector('input')
-    fireEvent.change(textField, {target: { value: "example task"}})
-    fireEvent.click(getByTestId('addButton'))
-    expect(dialog).toBeInTheDocument()
 })
 
 test("click Add button with filled text calls onAdd function with text", () => {
