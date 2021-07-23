861ed8ce5f235b4816cd361754311991874b3eba
Joseph Smith <joseph.smith@tessella.com>
2021-07-23 12:05:28 +0100

    Updated with add task PR changes

--- src/components/__tests__/AddTask.test.js
+++ src/components/__tests__/AddTask.test.js
@@ -18,29 +18,6 @@ test("input of new task string updates text field", () => {
     expect(textField.value).toBe("example task")
 })
 
-test("click Add button with filled text field closes dialog", () => {
-    // Passes but should fail as dialog should close //
-    const mock = jest.fn()
-    const { getByTestId } = render(<AddTask onAdd={mock}/>)
-    fireEvent.click(getByTestId('dialogButton'))
-    const dialog = getByTestId('dialog')
-    const textField = getByTestId('newTask').querySelector('input')
-    fireEvent.change(textField, {target: { value: "example task"}})
-    fireEvent.click(getByTestId('addButton'))
-    fireEvent.click(getByTestId('addButton'))
-    expect(dialog).toBeInTheDocument()
-})
-
-test("click Add button with filled text calls onAdd function", () => {
-    const mock = jest.fn()
-    const { getByTestId } = render(<AddTask onAdd={mock}/>)
-    fireEvent.click(getByTestId('dialogButton'))
-    const textField = getByTestId('newTask').querySelector('input')
-    fireEvent.change(textField, {target: { value: "example task"}})
-    fireEvent.click(getByTestId('addButton'))
-    expect(mock).toHaveBeenCalled()
-})
-
 test("click Add button with filled text calls onAdd function with text", () => {
     const mock = jest.fn()
     const { getByTestId } = render(<AddTask onAdd={mock}/>)
@@ -48,5 +25,7 @@ test("click Add button with filled text calls onAdd function with text", () => {
     const textField = getByTestId('newTask').querySelector('input')
     fireEvent.change(textField, {target: { value: "example task"}})
     fireEvent.click(getByTestId('addButton'))
+    expect(mock).toHaveBeenCalled()
     expect(mock).toHaveBeenCalledWith("example task")
 })
+//
