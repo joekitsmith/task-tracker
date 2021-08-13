907aefc8d2fad68cf90601e544d6a6a65f7e6d2d
Joseph Smith <joseph.smith@tessella.com>
2021-08-13 14:26:29 +0100

    actionCreator for completed state

--- src/state/action-creator/index.js
+++ src/state/action-creator/index.js
@@ -1,8 +1,19 @@
-export const addTaskToList = (task) => {
+const addTaskToList = (task) => {
     return(dispatch) => {
         dispatch({
             type: 'add',
             payload: task
         })
     }
-} 
+}
+
+const checkTaskAsCompleted = (id) => {
+    return(dispatch) => {
+        dispatch({
+            type: 'complete',
+            payload: id
+        })
+    }
+}
+
+export { addTaskToList, checkTaskAsCompleted }
