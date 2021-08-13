1d593c2823fc0916b06b78b1dded9d062ec2fffd
Joseph Smith <joseph.smith@tessella.com>
2021-08-13 14:26:13 +0100

    Reducer to change completed state

--- src/state/reducers/taskReducer.js
+++ src/state/reducers/taskReducer.js
@@ -1,12 +1,24 @@
 import initialState from '../initial-task-list.json'
 
+const removeIdFromArray = (array, id) => {
+    id = parseInt(id)
+    for(var i=0; i<array.length; i++) {
+        if(array[i].id == id) {
+            array[i].completed = !(array[i].completed)
+        }
+    }
+    return [...array]
+}
+
 const taskReducer = (
     state = initialState,
     action
 ) => {
     switch (action.type) {
         case "add":
-            return [...state, {id:state.length+1, text:action.payload}]
+            return [...state, {id:state.length+1, text:action.payload, completed:false}]
+        case "complete":
+            return removeIdFromArray(state, action.payload)
         default:
             return state
     }
